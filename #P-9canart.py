6663b8ea93c89755554b7a52dba6bdc9bfefd951
EliasT23 <l241050181@pabellon.tecnm.mx>
2024-12-06 14:48:04 -0600

    Delete #P-9canart.py

--- #P-9canart.py
+++
@@ -1,8 +0,0 @@
-num_articulos = int(input("Coloque el numero de articulos que decee comprar: "))
-
-if num_articulos > 3:
-    precio= num_articulos * 30
-    print("Muy bien usted compro:\t",num_articulos,"\nAl costo de $30 por articulo entonces usted pagara: \t"+str(precio))
-else:
-    precio= num_articulos * 45
-    print("Muy bien usted compro:\t",num_articulos,"\nAl costo de $30 por articulo entonces usted pagara: \t"+str(precio))
